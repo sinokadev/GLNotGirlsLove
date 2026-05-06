0076d9112b27680652462953913dea1eb8bb2508
sinokadev <sinoka@sinoka.dev>
2026-05-06 10:42:16 +0900

    add glad and glfw

--- justfile
+++ justfile
@@ -1,12 +1,14 @@
 default: build run
 
 setup:
+    cmake -S . -B build
+
+debug:
     cmake -S . -B build -D CMAKE_BUILD_TYPE=Debug
 
 build:
     cmake --build build
 
-# 실행
 run:
     ./build/GLNotGirlsLove
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,37 @@
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "GLNotGirlsLove", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
