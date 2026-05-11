27990d07e1cd305375d6d7b9b0362904a98e597d
sinokadev <sinoka@sinoka.dev>
2026-05-11 13:34:26 +0900

    add: window hint

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,9 @@ int main() {
         return -1;
     }
     glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
     if (!window) {
